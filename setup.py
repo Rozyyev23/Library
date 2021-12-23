92ade1a85b7de73f27b7149ab44331696a405eb8
Rozyyev23 <74649421+Rozyyev23@users.noreply.github.com>
2021-12-24 01:43:47 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 24 01:38:05 2021
+
+@author: Windows 10 Pro
+"""
+
+from setuptools import setup, find_packages
+long_description= '''Python module for optimization tasks'''
+setup(name='Library',
+      version='1.5',
+      url='https://github.com/Rozyyev23/Library',
+      license='MIT',
+      author='deaflurryth',
+      author_email='atarozyev2307@gmail.com',
+      description='Add static script_dir() method to Path',
+      zip_safe=False)
